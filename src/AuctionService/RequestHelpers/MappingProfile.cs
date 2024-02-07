c5d14d860f47db27a95c176167aa3b7f54fcb7fd
Emir Özdağ <emir.ozdag@hotmail.com>
2024-02-07 12:10:14 +0300

    End Of Section 2

---
+++ src/AuctionService/RequestHelpers/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AuctionService.Dtos;
+using AuctionService.Entites;
+using AutoMapper;
+
+namespace AuctionService;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(i => i.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>().ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
